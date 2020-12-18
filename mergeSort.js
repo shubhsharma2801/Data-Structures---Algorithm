4f874cf573dbff75bff8ca9199963fccb761c1ac
Shubham Sharma <shubhsharma2895@gmail.com>
2020-12-18 13:18:11 +0530

    Added Description

--- mergeSort.js
+++ mergeSort.js
@@ -1,3 +1,16 @@
+/** Merge Sort is classic case of Divide & merge algoritm.
+ *  In Divide and Conquor stratagey. We dive the problem into simpler parts which in turn are soloution to smaller problem.
+ *  And using conquor strategy we combine all the individual solution to form one bigger Solution.
+ *  In merge Sort Algorithm. We have two parts.
+ *  1. Divide & then merge 
+ *  2. Recursively divide the array into left and right subarray. Until only single element array is remaining. Which in itself is sorted.
+ *  3. After that we merge left & right subarray. Which are individually sorted.
+ *     We will take advantage of the fact that left and right subarrays are sorted.
+ *  4. In merging of two array. We usually maintain two pointer.
+ *     We check if array at position i < array at position j
+ *      then push the element at increment i
+ *     else increment j
+ */
 const merge = (left, right) => {
   let array = [];
   while (left.length && right.length) {
